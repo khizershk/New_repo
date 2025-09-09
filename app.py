def382770d40ea8610715f0216609de09d6c41bc
Khizershk <shaikhmohammed057@gmail.com>
2025-09-09 18:36:29 +0530

    Update app.py for prod1 deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(1, 1), 2)
+       
 
 if __name__ == "__main__":
     unittest.main()
